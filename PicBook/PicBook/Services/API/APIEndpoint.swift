b701cc06c1069425e09ac6932b9047c559a67633
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-04 22:17:29 +0600

    feat(api): add AppConfig and helper types for APIService
    Includes APIEndpoint, APIError, HTTPMethod, and HTTPHeader to support structured API requests and error handling.

---
+++ PicBook/PicBook/Services/API/APIEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  APIEndpoint.swift
+//  PicBook
+//
+//  Created by Md. Mahinur Rahman on 9/4/25.
+//
+
+import Foundation
+
+enum APIEndpoint {
+    case lists
+}
---
+++ PicBook/PicBook/Services/API/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  PicBook
+//
+//  Created by Md. Mahinur Rahman on 9/4/25.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get
+    case post
+    case put
+    case delete
+}
