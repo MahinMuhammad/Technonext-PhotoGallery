9f0435ace19f1e4102cb101d4a8c3da3ac3cd90e
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-04 20:48:52 +0600

    chore(project): initialize Xcode project in repo root

---
+++ PicBook/PicBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PicBook
+//
+//  Created by Md. Mahinur Rahman on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PicBook/PicBook/PicBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicBookApp.swift
+//  PicBook
+//
+//  Created by Md. Mahinur Rahman on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PicBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
