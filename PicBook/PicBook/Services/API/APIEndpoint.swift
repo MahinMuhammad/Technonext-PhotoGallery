4fc4272c3e1d800995e2e2281ae2df4edcafb273
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-04 22:31:07 +0600

    feat(api): implement /v2/list endpoint (GET, page/limit)

--- PicBook/PicBook/Services/API/APIEndpoint.swift
+++ PicBook/PicBook/Services/API/APIEndpoint.swift
@@ -8,5 +8,39 @@
 import Foundation
 
 enum APIEndpoint {
-    case lists
+    case getList(page: Int, limit: Int)
+}
+
+extension APIEndpoint {
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getList:
+            return .get
+        }
+    }
+    
+    var queryItems: [URLQueryItem]? {
+            switch self {
+            case let .getList(page, limit):
+                return [
+                    URLQueryItem(name: "page", value: String(page)),
+                    URLQueryItem(name: "limit", value: String(limit))
+                ]
+            }
+        }
+    
+    var path: String {
+        switch self {
+        case .getList:
+            return "/v2/list"
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self{
+        case .getList:
+            return nil
+        }
+    }
 }
