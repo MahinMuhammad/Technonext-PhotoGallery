8ecabc9f0065c48c4fb6e251169871e5a8fbd946
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-04 22:51:37 +0600

    feat(api): add PBAPIService (generic request, query items, api error handling)

--- PicBook/PicBook/Services/API/PBAPIService.swift
+++ PicBook/PicBook/Services/API/PBAPIService.swift
@@ -14,5 +14,58 @@ final class PBAPIService{
     
     typealias DataResponse = (data: Data, response: URLResponse)
     
+    //MARK: - Network Call Methods
     
+    /// Generic method for make  a request over network.
+    /// - Parameter endpoint: Enpoint that will the request will be made for
+    /// - Returns: A Model that is expected from the returned response data
+    public func gerenicRequest<T: Decodable>(endpoint: APIEndpoint) async throws -> T {
+        let request = try createRequest(with: endpoint)
+        let dataResponse: DataResponse = try await session.data(for: request)
+        return try responseHandler(dataResponse)
+    }
+    
+    //MARK: - Helper Methods
+    
+    /// Intercepts the network call and builds a request for that perticular endpoint
+    /// - Parameter endpoint: Enpoint that will the request will be made for
+    /// - Returns: A custom request for that perticular endpoint
+    private func createRequest(with endpoint: APIEndpoint) throws -> URLRequest{
+        var url = baseURL.appendingPathComponent(endpoint.path)
+        if let queryItems = endpoint.queryItems{
+            url = try addQueryItems(queryItems, to: url)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        if let headers = endpoint.headers{
+            request.allHTTPHeaderFields = headers
+        }
+        return request
+    }
+    
+    /// Adds query items (Parameters of url) to the url if thereis any
+    /// - Parameters:
+    ///   - items: URLQueryItems that will be added to the request
+    ///   - url: The url that includes base url and endpoint
+    /// - Returns: Url with the query items added
+    private func addQueryItems(_ items: [URLQueryItem], to url: URL) throws -> URL{
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        components?.queryItems = items
+        guard let resultURL = components?.url else {throw APIError.invalidURL}
+        return resultURL
+    }
+    
+    /// Handles the DataResponse accoding the the response statusCode
+    /// - Parameter dataResponse: DataResponse recieved from the network call
+    /// - Returns: A Model that is decoded from the data recieved over the network call
+    private func responseHandler<T: Decodable>(_ dataResponse: DataResponse) throws -> T{
+        guard let httpUrlResponse = dataResponse.response as? HTTPURLResponse else {throw APIError.invalidResponse}
+        
+        switch httpUrlResponse.statusCode{
+        case 200..<300:
+            return try decoder.decode(T.self, from: dataResponse.data)
+        default:
+            throw APIError.invalidHTTPStatus(httpUrlResponse.statusCode)
+        }
+    }
 }
