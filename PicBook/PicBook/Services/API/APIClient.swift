5959c30c71a7f959299e084c18b201c9db3823b4
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-05 14:15:30 +0600

    refactor(api): revert to async/await and remove Combine from network calls

--- PicBook/PicBook/Services/API/APIClient.swift
+++ PicBook/PicBook/Services/API/APIClient.swift
@@ -8,21 +8,6 @@
 import Foundation
 import Combine
 
-protocol APIClient {
+protocol APIClient: AnyObject {
     func gerenicRequest<T: Decodable>(endpoint: APIEndpoint) async throws -> T
-}
-
-/// We do not need the combine at all we could have used the built in async await
-/// Combine is just being used to meet the task requirement
-extension APIClient {
-    func requestPublisher<T: Decodable>(endpoint: APIEndpoint) -> AnyPublisher<T, Error> {
-        Deferred {
-            Future { promise in
-                Task {
-                    do { let v: T = try await self.gerenicRequest(endpoint: endpoint); promise(.success(v)) }
-                    catch { promise(.failure(error)) }
-                }
-            }
-        }.eraseToAnyPublisher()
-    }
 }
