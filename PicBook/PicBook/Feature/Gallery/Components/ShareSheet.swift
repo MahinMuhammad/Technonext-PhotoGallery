26ff5ddac1392904ccf90441458134c617d687f9
MahinMuhammad <amimahinurrahman@gmail.com>
2025-09-05 23:03:04 +0600

    feat(image): add share functionality for images

---
+++ PicBook/PicBook/Feature/Gallery/Components/ShareSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ShareSheet.swift
+//  PicBook
+//
+//  Created by Md. Mahinur Rahman on 9/5/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct ShareSheet: UIViewControllerRepresentable {
+    let items: [Any]
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: items, applicationActivities: nil)
+    }
+    func updateUIViewController(_ vc: UIActivityViewController, context: Context) {}
+}
